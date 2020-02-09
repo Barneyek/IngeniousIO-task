8a272e15429a51a3229df70094f7a646acd31c1b
Damian Osiecki <osieckidamian1992@gmail.com>
2020-02-09 22:36:14 +0100

    Add tablet menu

---
+++ src/js/menu.js
@@ -0,0 +1,13 @@
+const handleMenu = (button, menu) => {
+    button.addEventListener('click', () => {
+            button.classList.toggle('change');
+            menu.classList.toggle('show');
+            document.body.classList.toggle('lock'); 
+    })
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const buttonMenu = document.querySelector('.navbar__menu'),
+        menu = document.querySelector('.side-nav');
+    handleMenu(buttonMenu, menu);
+})
